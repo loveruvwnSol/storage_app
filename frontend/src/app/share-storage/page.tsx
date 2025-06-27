a1a8318cddde5eab75f29e8e3185f38c47be0730
sol <ocosio0907@gmail.com>
2025-06-27 18:46:41 +0900

    add some page file

---
+++ frontend/src/app/share-storage/page.tsx
@@ -0,0 +1,11 @@
+import { Sidebar } from "@/components/organisms/Sidebar";
+
+const Index = () => {
+  return (
+    <div className="flex w-full h-full bg-gray-50">
+      <Sidebar />
+    </div>
+  );
+};
+
+export default Index;
